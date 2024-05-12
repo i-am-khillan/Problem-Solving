9f1b728598e56b8d0100866e866ea7bf42bd3439
i-am-khillan <khillan.i.verma@gmail.com>
2024-05-12 13:00:11 +0530

    Add files via upload
    commit changes

---
+++ src/CountOfPairsSum.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountOfPairsSum {
+    public static void main(String[] args) {
+
+    }
+    public static int countOfPairs(int arr[], int sum){
+        Map<Integer,Integer> map=new HashMap<>();
+
+        return 0;
+    }
+}
