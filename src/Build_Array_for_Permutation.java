6e12392e27bd986c05bd56d5fb683fcc9c5f5812
“i-am-khillan” <khillan.i.verma@gmail.com>
2024-05-13 21:43:41 +0530

    first commit

---
+++ src/Build_Array_for_Permutation.java
@@ -0,0 +1,20 @@
+public class Build_Array_for_Permutation {
+
+
+    public static void main(String[] args) {
+        int arr[] = //{5, 0, 1, 2, 3, 4};
+        {0,2,1,5,3,4};
+        int[]ss=buildArray(arr);
+        for (int a : ss) {
+            System.out.println(a);
+
+        }
+    }
+    public static int[] buildArray(int nums[]){
+        int []arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[nums[i]];
+        }
+        return arr;
+    }
+}
